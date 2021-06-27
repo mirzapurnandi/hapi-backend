6977d395717efafdea74dacec1f034f9f3445b40
mirza purnandi <mirza.purnandi@gmail.com>
2021-06-27 08:06:27 +0700

    tambah fungsi getBook berdasarkan bookId

--- src/routesBook.js
+++ src/routesBook.js
@@ -1,4 +1,4 @@
-const { addBookHandler, getAllBooksHandler } = require('./handlerBook');
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler } = require('./handlerBook');
 
 const routes = [{
   method: 'POST',
@@ -15,7 +15,7 @@ const routes = [{
 {
   method: 'GET',
   path: '/books/{bookId}',
-  handler: () => {},
+  handler: getBookByIdHandler,
 },
 
 {
