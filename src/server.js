93b0e2870b9a0b922188b520a22d896dbc2fd47d
mirza purnandi <mirza.purnandi@gmail.com>
2021-06-27 07:57:45 +0700

    menambahkan routeBook dan HandlerBook

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const routeBooks = require('./routesBook');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5001,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
@@ -11,7 +12,10 @@ const init = async () => {
       },
     },
   });
+
   server.route(routes);
+  server.route(routeBooks);
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
